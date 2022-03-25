308a7ab61b944e0a1cb088de7367dff5f56a426f
legion <88172317+LegacyOO7@users.noreply.github.com>
2022-03-25 12:38:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/LegacyOO7/akira-mediasearch /IMDb-Movie-Bot
-cd /IMDb-Movie-Bot
+git clone -b master https://github.com/LegacyOO7/akira-mediasearch /akira-mediasearch
+cd /akira-mediasearch
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
